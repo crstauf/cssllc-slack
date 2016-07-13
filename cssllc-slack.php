015aa6fff7655f64e3dc9df171270c1e6e7b5eba
Caleb Stauffer <develop@calebstauffer.com>
2016-07-12 23:49:12 -0400

    add security

--- cssllc-slack.php
+++ cssllc-slack.php
@@ -7,6 +7,12 @@ Description: Integration plugin for WordPress projects to Slack
     Version: 0.0.3
 **/
 
+if (!defined('ABSPATH') || !function_exists('add_filter')) {
+	header( 'Status: 403 Forbidden' );
+	header( 'HTTP/1.1 403 Forbidden' );
+	exit();
+}
+
 register_deactivation_hook(__FILE__,array('cssllc_slack','on_deactivation'));
 
 if (apply_filters('cssllc_slack_php_errors',false)) {
